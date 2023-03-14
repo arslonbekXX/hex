213d70bbc56e92104663dc085158b8d20f47bed6
arslonbekXX <arslonbek.lion067@gmail.com>
2023-03-14 17:30:14 +0500

    add script codes

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Hex App");
+const countInput = document.getElementById("count");
+const generateBtn = document.querySelector(".btn");
+
+generateBtn.addEventListener("click", () => {
+	const value = countInput.value || "";
+	if (value.trim() === "") {
+		alert("togri kirit ey");
+		countInput.value = "";
+	} else {
+		console.log("value = ", value);
+	}
+});
